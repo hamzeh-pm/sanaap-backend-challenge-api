8275fb85099778061948436cea65138b2030aee0
Hamzeh Pourmahdi <h.pourmahdi@hotmail.com>
2025-09-26 12:52:50 +0330

    fix: fix a bug on pytest not running the migration successfully, implement post migration signal for consistent result in assigning permissions to roles

---
+++ sanaap_backend_challenge_api/documents/signals.py
@@ -0,0 +1,37 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+@receiver(post_migrate)
+def assign_document_permissions_to_roles(sender, **kwargs):
+    if sender.name != "sanaap_backend_challenge_api.documents":
+        return
+
+    document_ct = ContentType.objects.get(app_label="documents", model="document")
+
+    add_permission = Permission.objects.get(
+        codename="add_document", content_type=document_ct
+    )
+    change_permission = Permission.objects.get(
+        codename="change_document", content_type=document_ct
+    )
+    delete_permission = Permission.objects.get(
+        codename="delete_document", content_type=document_ct
+    )
+    view_permission = Permission.objects.get(
+        codename="view_document", content_type=document_ct
+    )
+
+    admin_group = Group.objects.get(name="Admin")
+    editor_group = Group.objects.get(name="Editor")
+    viewer_group = Group.objects.get(name="Viewer")
+
+    admin_group.permissions.add(
+        add_permission, change_permission, delete_permission, view_permission
+    )
+    editor_group.permissions.add(add_permission, change_permission, view_permission)
+    viewer_group.permissions.add(view_permission)
+
+    print("Document permissions assigned to roles successfully.")
