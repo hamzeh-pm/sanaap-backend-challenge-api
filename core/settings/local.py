3aefc706030ad0e28b3b539e97b73faae1e69ce0
Hamzeh Pourmahdi <h.pourmahdi@hotmail.com>
2025-09-27 11:07:24 +0330

    chors: increase jwt token ttl for local development

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,6 +1,7 @@
 from .base import *  # noqa: F403
 from .base import INSTALLED_APPS
 from .base import env
+from datetime import timedelta
 
 DEBUG = True
 
@@ -11,3 +12,11 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 
 # django-extensions
 INSTALLED_APPS += ["django_extensions"]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+}
